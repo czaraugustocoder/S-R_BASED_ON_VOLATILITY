e760aa3c5efaa9bc136f54fe242d8ebbce1f8c00
Cesar Augusto <108474749+czaraugustocoder@users.noreply.github.com>
2024-07-14 19:46:12 -0300

    Update app.py teste de data

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ else:
     ticker1 = acao_cod
 
 print(ticker1)
-df1 = yf.download(ticker1, "2012-01-01", str(str(datetime.today().year)+"-"+str(datetime.today().month)+"-"+str(datetime.today().day)))
+df1 = yf.download(ticker1, "2012-01-01", "2024-07-12")
 df1["Returns"] = df1["Adj Close"].pct_change(1)
 df1["Adj Low"] = df1["Low"] - (df1["Close"]-df1["Adj Close"])
 df1["Adj High"] = df1["High"] - (df1["Close"]-df1["Adj Close"])
